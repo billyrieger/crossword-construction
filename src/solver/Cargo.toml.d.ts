82e6585a0891cf02b56c7d9dfe0e3f3f38fa6ec3
Billy Rieger <wrieger@protonmail.com>
2021-03-27 12:01:21 -0400

    Redo

--- src/solver/Cargo.toml.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "../target/wasm-pack/solver/index";
-
-type Exports = typeof import("../target/wasm-pack/solver/index");
-declare const init: () => Promise<Exports>;
-export default init;
