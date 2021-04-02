3a4873010606d3c6c0404bcf8586273ec432eeb1
Billy Rieger <wrieger@protonmail.com>
2021-04-01 23:35:28 -0400

    Svelte hello world

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,9 +1,26 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: "svelte-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".svelte"],
+  },
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  mode: "production",
 };
