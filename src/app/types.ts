683f7e16f7d41bab8eeaeab47a82cd9c4a3a7710
Billy Rieger <wrieger@protonmail.com>
2021-04-10 11:32:40 -0400

    Add some utility types

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,5 +1,20 @@
-export type CellType = {
-    id: number;
+export type Maybe<T> = T | undefined;
+
+export type DeepReadonly<T> = T extends (infer R)[]
+    ? DeepReadonlyArray<R>
+    : T extends Function
+    ? T
+    : T extends object
+    ? DeepReadonlyObject<T>
+    : T;
+
+interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+
+type DeepReadonlyObject<T> = {
+    readonly [P in keyof T]: DeepReadonly<T[P]>;
+};
+
+export interface CellType {
     number?: number;
     value?: string;
 };
